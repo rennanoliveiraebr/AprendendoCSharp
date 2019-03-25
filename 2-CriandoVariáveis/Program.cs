48e0f850e75e37193d6b75f41cc69b5fa16c7ed0
Rennan Oliveira <rennan.oliveira@excelbr.com.br>
2019-03-25 09:14:34 -0300

    Alterações iniciais

---
+++ 2-CriandoVariáveis/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_CriandoVariáveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 2: Criando Variáveis");
+
+            int idade;
+            idade = 32;
+
+            Console.WriteLine(idade);
+
+            idade = 10;
+            Console.WriteLine(idade);
+
+            idade = 10 + 5;
+            Console.WriteLine(idade);
+
+            idade = (10 + 5) * 2;
+            Console.WriteLine(idade + " Né");
+
+            Console.WriteLine("Execução finalizada. Tecle enter para sair...");
+            Console.ReadLine();
+        }
+    }
+}
