32c94b278a4c4d5ce5effd167bacfd15d1a0834b
Rennan Oliveira <rennan.oliveira@excelbr.com.br>
2019-03-26 14:17:20 -0300

    Inclusão das aulas 6, 7 e 8

---
+++ 6-AtribuicoesVariaveis/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_AtribuicoesVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 6 - Atribuições de Variáveis");
+
+            int idade = 32;
+            int idadeGustavo = idade;
+
+            idade = 20;
+
+            Console.WriteLine(idade);
+            Console.WriteLine(idadeGustavo);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 7-Condicionais/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 7 - Condicionais");
+
+            int idadeJoao = 16;
+            int quantidadePessoas = 2;
+
+            if (idadeJoao >= 18)
+            {
+                Console.WriteLine("João é 18+");
+            }
+            else
+            {
+                if (quantidadePessoas >= 2)
+                {
+                    Console.WriteLine("João não é 18+, mas está acompanhado");
+                }
+                else
+                {
+                    Console.WriteLine("João não é 18+");
+                }
+                
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
