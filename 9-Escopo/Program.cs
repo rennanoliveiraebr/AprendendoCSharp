89585b48c2795ea685a40d0a5f2595484d048f6e
Rennan Oliveira <rennan.oliveira@excelbr.com.br>
2019-03-26 14:51:57 -0300

    Incluídas aula 9 e exercício 9.1

---
+++ 9-Escopo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 9 - Escopo");
+
+            int idadeJoao = 18;
+            bool acompanhado = true;
+            string mensagemAdicional;
+
+            if (acompanhado == true)
+            {
+                mensagemAdicional = "João está acompanhado";
+                Console.WriteLine(mensagemAdicional);
+            }
+            else
+            {
+                mensagemAdicional = "João não está acompanhado";
+                Console.WriteLine(mensagemAdicional);
+            }
+
+            if (idadeJoao >= 18 || acompanhado == true)
+            {
+                Console.WriteLine("João pode entrar");
+                Console.WriteLine(mensagemAdicional);
+            }
+            else
+            {
+                Console.WriteLine("João não pode entrar");
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 9.1-Exercício1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._1_Exercício1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario = 3000.00;
+
+            double taxa;
+            int valor;
+
+            if(salario >= 1900.00 && salario <= 2800.00)
+            {
+                taxa = 7.5;
+                valor = 142;
+                Console.WriteLine("A taxa é de: " + taxa + "%");
+                Console.WriteLine("O valor é de: R$" + valor);
+            }
+            else if(salario >= 2800.01 && salario <= 3751.00)
+            {
+                taxa = 15;
+                valor = 350;
+                Console.WriteLine("A taxa é de: " + taxa + "%");
+                Console.WriteLine("O valor é de: R$" + valor);
+            }
+            else if (salario >= 3751.01 && salario <= 4664.00)
+            {
+                taxa = 22.5;
+                valor = 636;
+                Console.WriteLine("A taxa é de: " + taxa + "%");
+                Console.WriteLine("O valor é de: R$" + valor);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
