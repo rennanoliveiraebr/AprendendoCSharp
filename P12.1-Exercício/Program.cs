c294d9c2c881000521b5f67ac0ba5e0649dbd92a
Rennan Oliveira <rennan.oliveira@excelbr.com.br>
2019-03-27 16:38:49 -0300

    Incluídas aulas de 10 a 13

---
+++ P12.1-Exercício/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P12._1_Exercício
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int contador = 1; contador <= 10; contador++)
+            {
+                Console.WriteLine(contador);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
