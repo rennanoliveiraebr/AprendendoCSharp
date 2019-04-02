1c3d2459e152fcae77f5c7ed9c1b3ac58e8881d5
Rennan Oliveira <rennan.oliveira@excelbr.com.br>
2019-04-02 09:25:09 -0300

    Finalização da aula 5 do módulo 2

---
+++ Aula 2/06-ByteBank/ContaCorrente.cs
@@ -0,0 +1,64 @@
+﻿//using _05_ByteBank; Como está compartilhando namespace, não é necessário declarar esse código
+
+namespace _06_ByteBank
+{
+    class ContaCorrente
+    {
+
+        public Cliente Titular { get; set; }
+        public int Agencia { get; set; }
+        public int Numero { get; set; }
+                
+
+        private double _saldo = 100;
+                     
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+
+                _saldo = value;
+            }
+        }
+              
+        public bool Sacar(double valor)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+
+            _saldo -= valor;
+            return true;
+
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+
+            _saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+
+        }
+
+    }
+}
+
